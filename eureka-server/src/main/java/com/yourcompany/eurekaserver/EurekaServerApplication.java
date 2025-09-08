9d66813053390e0da07291947ea7692b8d67e4c1
Harsh Bhagat <164932416+bhagatharsh123@users.noreply.github.com>
2025-09-08 11:03:59 +0530

    Add files via upload

---
+++ eureka-server/src/main/java/com/yourcompany/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.yourcompany.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+        System.out.println("eureka server started...........");
+        }
+}
